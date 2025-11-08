8442daf7530ce3e9f111f6b854cc4c0776765060
MinervaRubi <Minervarubirodriguez@gmail.com>
2025-11-08 17:41:34 -0600

    recomendaciones

--- database/migrations/2025_11_08_233909_create_producto_recomendacion_table.php
+++ database/migrations/2025_11_08_233909_create_producto_recomendacion_table.php
@@ -6,22 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('producto_recomendacion', function (Blueprint $table) {
+        Schema::create('recomendaciones', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('nutricionista_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('cuestionario_id')->constrained('cuestionarios')->onDelete('cascade');
+            $table->text('comentarios')->nullable();
+            $table->dateTime('fecha');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('producto_recomendacion');
+        Schema::dropIfExists('recomendaciones');
     }
 };
