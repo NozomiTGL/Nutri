654bd8892fc1e09b8f6b5f5b46649fcd90644f5a
Sofmer7 <promyios27@gmail.com>
2025-11-10 18:27:43 -0600

    pepe2

---
+++ app/Models/Articulo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Articulo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['titulo', 'contenido', 'imagen', 'autor_id', 'fecha'];
+
+    public function autor() {
+        return $this->belongsTo(Usuario::class, 'autor_id');
+    }
+}
+
